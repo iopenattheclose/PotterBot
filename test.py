7e7f65afc425de26c57cdcbc0d2a8d91d496f923
iopenattheclose <prupro69@gmail.com>
2025-01-29 22:49:25 +0530

    base model

--- test.py
+++ test.py
@@ -6,19 +6,15 @@ import pandas
 def start():
     # prompt format
     print("Start/n")
-    intstructions_string = f"""ShawGPT, functioning as a virtual data science consultant on YouTube, communicates in clear, accessible language, escalating to technical depth upon request. \
-    It reacts to feedback aptly and ends responses with its signature '–ShawGPT'. \
-    ShawGPT will tailor the length of its responses to match the viewer's comment, providing concise acknowledgments to brief expressions of gratitude or feedback, \
-    thus keeping the interaction natural and engaging.
-
-    Please respond to the following comment.
-    """
+    intstructions_string = f"""I am insufferable know it all, ask away you little tyke!!"""
     print("prompt builder/n")
-    prompt_builder = lambda comment: f'''<s>[INST] {intstructions_string} \n{comment} \n[/INST]\n'''
+    prompt_builder = lambda comment: f'''{intstructions_string} \n{comment} \n'''
 
     model_path = "mlx-community/Mistral-7B-Instruct-v0.2-4bit"
-    prompt = prompt_builder("name the 7 hocruxes in two lines?")
-    max_tokens = 140
+    print("enter your question")
+    input_prompt = input()
+    prompt = prompt_builder({input_prompt})
+    max_tokens = 256
 
     model, tokenizer = load("mlx-community/Mistral-7B-Instruct-v0.2-4bit")
     response = generate(model, tokenizer, prompt=prompt, max_tokens = max_tokens,verbose=True)
